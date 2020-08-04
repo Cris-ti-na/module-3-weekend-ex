1eedc7e35c28950c82f717bc1c51661391dcb9cb
Cris-ti-na <cristina.ccmm@gmail.com>
2020-08-04 22:05:23 +0200

    new folder docs

---
+++ docs/precache-manifest.e87cf98ddbade178c0f226831e915fd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479123ade749c9d0d695a5007662f8f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f63dc308e3c2830bf4e1",
+    "url": "/static/css/main.8ef5e641.chunk.css"
+  },
+  {
+    "revision": "15ca3c4eaccb3a9de74b",
+    "url": "/static/js/2.f71ea0ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f71ea0ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63dc308e3c2830bf4e1",
+    "url": "/static/js/main.a7207acf.chunk.js"
+  },
+  {
+    "revision": "2ae6e346d186b5e5b788",
+    "url": "/static/js/runtime-main.ca8a46de.js"
+  }
+]);
