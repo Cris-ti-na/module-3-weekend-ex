91ae0c00ca6759697638a4afd2a06c1b57848957
Sergio Arbeo <serabe@gmail.com>
2020-08-05 14:14:34 +0200

    Aquí hay varias cosicas:
    1. El constructor estaba mal, ya que tienes que pasar al padre todas las
       props.
    2. Al hacerlo así, el constructor pasa a ser innecesario, ya que pasar
       al padre todos los argumentos es el funcionamiento por defecto.
    3. En vez de hacer algo estilo `a.b.c ? a.b.c : 'other'`, podemos usar
       también el encadenamiento opcional (u operador Elvis) https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Operadores/Encadenamiento_opcional
    4. Complementamos el operador Elvis con el nullish coalescing (`??`) https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
    5. En un punto has usado `class` en vez de `className`
    6. Renombramos la propiedad de `Show` de `id` a `key` ya que necesitamos
       que la colección lo tenga.

--- src/components/showList/ShowList.js
+++ src/components/showList/ShowList.js
@@ -1,30 +1,27 @@
 import React from "react";
 import Show from "../show/Show.js";
 
-const altImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV-SHOW'
-const altRating = 'no rating'
+const altImage =
+  "https://via.placeholder.com/210x295/ffffff/666666/?text=TV-SHOW";
+const altRating = "no rating";
 
 class ShowList extends React.Component {
-    constructor({list}) {
-        super(list);
-    }
-
-    render() {
-        console.log(this.props);
-        return(
-            <ul class="show__list">
-                {this.props.showList.map((item) =>
-                <Show
-                    id={item.show.id}
-                    image={item.show.image ? item.show.image.original : altImage}
-                    name={item.show.name}
-                    rating={item.show.rating.average ? item.show.rating.average : altRating}
-                    status={item.show.status}
-                />
-                )}
-            </ul>
-        );
-    }
+  render() {
+    console.log(this.props);
+    return (
+      <ul className="show__list">
+        {this.props.showList.map((item) => (
+          <Show
+            key={item.show.id}
+            image={item.show.image?.original ?? altImage}
+            name={item.show.name}
+            rating={item.show.rating?.average ?? altRating}
+            status={item.show.status}
+          />
+        ))}
+      </ul>
+    );
+  }
 }
 
 export default ShowList;
