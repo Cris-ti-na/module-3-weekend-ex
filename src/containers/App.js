fdeae2e5d3f4f607159bcde9ea5bfcd6562b3257
Sergio Arbeo <serabe@gmail.com>
2020-08-05 14:09:01 +0200

    Dado que estamos usando attributos de clase con arrow functions, no es necesario que hagamos el bind.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,18 @@
 import React from "react";
-import '../scss/main.scss';
-import ShowList from '../components/showList/ShowList.js';
-import Filter from '../components/filter/Filter.js';
+import "../scss/main.scss";
+import ShowList from "../components/showList/ShowList.js";
+import Filter from "../components/filter/Filter.js";
 
 //SHOWLIST
-const API_DATA = 'http://api.tvmaze.com/search/shows'
+const API_DATA = "http://api.tvmaze.com/search/shows";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       showList: [],
-      inputText: '',
+      inputText: "",
     };
-
-    this.getDataFromApi = this.getDataFromApi.bind(this);
-    this.filterHandler = this.filterHandler.bind(this);
   }
 
   componentDidMount() {
@@ -23,36 +20,42 @@ class App extends React.Component {
   }
 
   getDataFromApi = () => {
-    return fetch(API_DATA + '?q=girls') //girls = ${inputName}
+    return fetch(API_DATA + "?q=girls") //girls = ${inputName}
       .then((response) => response.json())
       .then((responseData) => {
         console.log(responseData);
-        this.setState({showList: responseData});
+        this.setState({ showList: responseData });
       });
-  }
+  };
 
   filterHandler = (ev) => {
     console.log(ev.currentTarget.value);
-    this.setState({inputText: ev.currentTarget.value});
-  }
+    this.setState({ inputText: ev.currentTarget.value });
+  };
 
   render() {
     const inputText = this.state.inputText;
     const unfilteredShows = this.state.showList;
-    const filteredShows = unfilteredShows.filter(serie =>
-      inputText ? serie.show.name.toLowerCase().includes(inputText.toLowerCase()) : true
+    const filteredShows = unfilteredShows.filter((serie) =>
+      inputText
+        ? serie.show.name.toLowerCase().includes(inputText.toLowerCase())
+        : true
     );
 
     console.log(filteredShows);
 
-    return(
+    return (
       <div className="App">
         <Filter filterHandler={this.filterHandler} />
-        {unfilteredShows.length ? <ShowList showList=
-          {filteredShows ? filteredShows : unfilteredShows}
-        /> : <p>CARGANDO</p>}
+        {unfilteredShows.length ? (
+          <ShowList
+            showList={filteredShows ? filteredShows : unfilteredShows}
+          />
+        ) : (
+          <p>CARGANDO</p>
+        )}
       </div>
-    )
+    );
   }
 }
 
